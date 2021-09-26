d80533b2b71d5a15c2cc8ae622096db503582abe
natyarteagac <natalia.arteagac94@gmail.com>
2021-09-26 17:06:58 -0500

    Task 3 with all requirements#

---
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -0,0 +1,24 @@
+$(() => {
+  let amenitieIds = {}
+  $('input[type=checkbox]').change(function () {
+    if ($(this).is(':checked')) {
+      amenitieIds[($(this).attr("data-id"))] = $(this).attr("data-name")
+      console.log("Checkbox is checked...")
+    } else if (!$(this).prop("checked")) {
+      delete amenitieIds[($(this).attr("data-id"))]
+    }
+    if (Object.keys(amenitieIds).length === 0) {
+      $("div.amenities h4").html("");
+    } else {
+      $("div.amenities h4").text(Object.values(amenitieIds).join(", "));
+    }
+  });
+})
+
+$.get('http://192.168.10.14:5001/api/v1/status/', function (data) {
+  if (data.status === 'OK') {
+    $('div#api_status').addClass('available');
+  } else {
+    $('div#api_status').removeClass('available');
+  }
+});
